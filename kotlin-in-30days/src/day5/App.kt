2d0086fd34ce2dd40a57b885316c470a03a8e531
timmy <timmybeeflin@gmail.com>
2020-09-14 17:12:06 +0800

    day 5 example

---
+++ kotlin-in-30days/src/day5/App.kt
@@ -0,0 +1,34 @@
+package day5
+
+fun main() {
+    callSum()
+
+    overload()  // overloading
+    overload(1)  // overloading num: 1
+    overload(2, false) // overloading num: 2, is true: false
+    overload(isTrue = true, num = 10) // overloading num: 10, is true: true
+}
+
+// default values
+fun sum(a: Int, b: Int, c: Int = 10) = a + b + c
+
+fun callSum() {
+    println(sum(1, 2, 3)) // 6
+    println(sum(1, 2)) // 13
+    println(sum(b = 2, c = 1, a = 5)) // 8
+}
+
+fun overload() {
+    println("overloading")
+}
+fun overload(num: Int) {
+    println("overloading num: $num")
+}
+fun overload(num: Int, isTrue: Boolean) {
+    println("overloading num: $num, is true: $isTrue")
+}
+
+fun todoTest() {
+    TODO("this is todo")
+    println("todo")
+}
